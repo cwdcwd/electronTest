7c57ddb59e8ef4a62cf8971fce1e7d24629fbd8c
CWD <cwd@cloudspokes.com>
2016-03-17 14:07:45 -0400

    setting up a test to report battery level

---
+++ electronTest.ino
@@ -0,0 +1,31 @@
+int getBatteryLevel(String command);
+
+float fBattery=0;
+float fBatteryLast=0;
+FuelGauge fuel;
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Starting up");
+  Particle.variable("batteryLevel", fBattery);
+  bool success = Particle.function("batteryLevel", getBatteryLevel);
+}
+
+void loop() {
+  fBattery=fuel.getSoC();
+
+  if(fBatteryLast!=fBattery){
+    Serial.print("Battery Level Changed!");
+    Serial.println(fBattery);
+    Particle.publish("BatteryLevel",String(fBattery));
+  }
+
+  fBatteryLast=fBattery;
+  delay(500);
+}
+
+int getBatteryLevel(String command){
+  Serial.println("Fetching battery level");
+  return (int)fBattery;
+}
